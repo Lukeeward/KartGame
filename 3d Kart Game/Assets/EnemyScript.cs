a785f7b694470c9786a6424ffa4cffc480c4ca2d
Luke <luke__ward@hotmail.co.uk>
2017-03-19 16:16:00 +0000

    new scripts and assests

---
+++ 3d Kart Game/Assets/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	private Vector3 moveDirection = Vector3.zero;
+	public CharacterController enemyController;
+	private float speed = 250.0F;
+	public float jumpSpeed = 11.0F;
+	public float gravity = 20.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = new Vector3 (-5, 0, 0);
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= speed;
+		moveDirection.y -= gravity * Time.deltaTime;
+		enemyController.Move (moveDirection * Time.deltaTime);
+	}
+}
---
+++ 3d Kart Game/Assets/LeaderboardScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using System;
+
+public class LeaderboardScript : MonoBehaviour {
+	public string leaderboard;
+
+	void Start ()
+	{
+		PlayGamesPlatform.DebugLogEnabled = true;
+	}
+
+	/// <summary>
+	/// Login In Into Your Google+ Account
+	/// </summary>
+	public void LogIn (bool ShowLeaderboard = false)
+	{
+		try{
+		Social.localUser.Authenticate ((bool success) =>
+			{
+				if (success) {
+					if(ShowLeaderboard)
+					{
+						OnShowLeaderBoard();
+					}
+				} else {
+				}
+			});
+		} catch(Exception ex) {
+			Debug.Log (ex);
+		}
+	}
+
+	/// <summary>
+	/// Shows All Available Leaderborad
+	/// </summary>
+	public void OnShowLeaderBoard ()
+	{
+		if (PlayGamesPlatform.Instance.IsAuthenticated()) {
+			((PlayGamesPlatform)Social.Active).ShowLeaderboardUI (leaderboard);
+		} else {
+			LogIn (true);
+		}
+	}
+
+	/// <summary>
+	/// Adds Score To leader board
+	/// </summary>
+	public void OnAddScoreToLeaderBoard (float newScore)
+	{
+		if (PlayGamesPlatform.Instance.IsAuthenticated()) {
+			Social.ReportScore ((int)newScore, leaderboard, (bool success) =>
+				{
+					if (success) {
+						Debug.Log ("Update Score Success");
+					} else {
+						Debug.Log ("Update Score Fail");
+					}
+				});
+		}
+	}
+}
